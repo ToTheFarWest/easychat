5189ce1d3c728f3f928b28472e3c5f1d6708b273
ToTheFarWest <yardenakin@gmail.com>
2019-03-25 11:41:20 +0200

    Create MainFrame.py
    School computers are gay

---
+++ client/MainFrame.py
@@ -0,0 +1,15 @@
+import Tkinter
+from Tkinter import *
+
+root = Tk()
+MainFrame = Frame(root, height = 1080, width = 1900)
+MainFrame.pack()
+
+top = Tkinter.Tk()
+B = Tkinter.Button(top, text ="Hello")
+
+B.pack()
+top.mainloop()
+
+
+root.mainloop()
